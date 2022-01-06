936897b73a06edc97415479edd89df97897ebc0c
Tin Dizdarevic <tinovmail@gmail.com>
2022-01-06 01:28:08 +0100

    Implement initial recipe list.

--- angular-the-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-the-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a test',
+      'https://image.freepik.com/free-photo/delicious-vietnamese-food-including-pho-ga-noodles-spring-rolls-white-table_181624-34062.jpg'
+    )
+  ]
   constructor() { }
 
   ngOnInit(): void {
